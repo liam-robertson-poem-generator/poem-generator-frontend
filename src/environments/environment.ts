6f93e7a638aa87a44f6f5417204091ca32870a81
Liam Robertson <liamjohnrobertson@gmail.com>
2021-11-25 10:46:22 +0000

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'syllabary-poem-generator',
+    appId: '1:775152054501:web:85507e1553f3c31bba640c',
+    databaseURL: 'https://syllabary-poem-generator-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'syllabary-poem-generator.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAEJF2n0uErPZkyWqC_qu63fDTUdbDM36Y',
+    authDomain: 'syllabary-poem-generator.firebaseapp.com',
+    messagingSenderId: '775152054501',
+    measurementId: 'G-TVG5K0ZVSV',
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
