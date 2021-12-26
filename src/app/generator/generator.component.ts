d5541e19461e7b3edfa06380d82aae961129a603
Liam-Robertson <liamjohnrobertson@gmail.com>
2021-12-26 14:25:45 +0000

    removed business logic from frontend

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -3,7 +3,6 @@ import { Observable } from "rxjs";
 import { startWith, map } from "rxjs/operators";
 import { AppService } from "../app.service";
 import { FormControl, Validators, FormGroup } from '@angular/forms';
-import { IPoem } from "./models/poem";
 import { Router } from "@angular/router";
 
 @Component({
@@ -14,22 +13,14 @@ import { Router } from "@angular/router";
 })
 
 export class GeneratorComponent implements OnInit {
-	
-	poemDataBool: boolean = true;
-	formBool: boolean = false;
-
-	dropdownOptions: string[];
+	poemFormGroup: FormGroup;
+	poemDataBool: boolean;
+	formBool: boolean;
+	poemCodeList: number[][];
 	filteredDropdownOptions: Observable<string[]>;
-
-	startingPoem: number[];
+	startingPoem: string;
 	numOfPoems: number;
 	poemOrder: string;
-
-	poemFormGroup: FormGroup;
-	poemCodeList: number[][];
-	currentPoemXmlUrl: string;
-	currentGlyphUrl: string;
-	poemNameList: string[];
 
 	constructor(
 		private appService: AppService,
@@ -37,22 +28,22 @@ export class GeneratorComponent implements OnInit {
 		) { }
 
   async ngOnInit() {
-		this.appService.getPoemNameList().subscribe(poemCodeListUnsorted => {
+		this.appService.getPoemNameList().subscribe(poemCodeList => {
 			
 			this.poemFormGroup = new FormGroup ({
 				startingPoemControl: new FormControl(''),
 				poemOrderControl: new FormControl(''),
-				numOfPoemsControl: new FormControl('', [Validators.min(0), Validators.max(poemCodeListUnsorted.length)])
+				numOfPoemsControl: new FormControl('', [Validators.min(0), Validators.max(poemCodeList.length)])
 			});
 
 			this.poemDataBool = false;
 			this.formBool = true; 
 					
-			this.poemCodeList = this.sortByMultipleValues(poemCodeListUnsorted);	
-
 			const dropdownOptions: string[] = this.poemCodeList.map(value => value.join("-").toString())
-			this.filteredDropdownOptions = this.poemFormGroup.get('startingPoemControl')!.valueChanges
-			.pipe(startWith(''), map(value => this._filter(value, dropdownOptions)));			
+
+			this.filteredDropdownOptions = this.poemFormGroup
+				.get('startingPoemControl')!.valueChanges
+				.pipe(startWith(''), map(value => this._filter(value, dropdownOptions)));			
 		});
 
   }
@@ -60,172 +51,11 @@ export class GeneratorComponent implements OnInit {
 	async execute() {		
 		this.router.navigate(["/loading"])
 
-		const startingPoemRaw: string = this.poemFormGroup.value.startingPoemControl
-		this.startingPoem = startingPoemRaw.split('-').map(coord => parseInt(coord))
+		this.startingPoem= this.poemFormGroup.value.startingPoemControl
 		this.numOfPoems = this.poemFormGroup.value.numOfPoemsControl
 		this.poemOrder = this.poemFormGroup.value.poemOrderControl
 
-		const finalPoemList = this.iterateBySyllables(this.poemCodeList, this.startingPoem, this.numOfPoems, this.poemOrder)
-		
-		const templateList = await this.createTemplateList(finalPoemList)
-		this.writeDocument(templateList)
-
 		this.router.navigate(["/success"])
-	}
-
-	iterateBySyllables(poemList: number[][], startingPoem: number[], numOfPoems: number, poemOrder: string) {
-		let [uniqueCoordList1, uniqueCoordList2, uniqueCoordList3]: number[][] = this.updateUniqueLists(poemList, startingPoem);
-
-		const nextAxisNumberDict: Map<number, number> = new Map([[0, 1], [1, 2], [2, 0]]);
-		let outputList: number[][] = [];
-		let successCounter: number = 0
-		let [xLoopCounter, yLoopCounter, zLoopCounter]: number[] = [0, 0, 0];
-		let currentAxisNumber: number = 0;
-
-		let currentXUniqueList: number[]
-		let currentYUniqueList: number[]
-		let currentZUniqueList: number[]
-		let currentXCoord: number
-		let currentYCoord: number
-		let currentZCoord: number
-		let currentTargetPoem: number[]
-
-		while (successCounter < numOfPoems) {
-			const axisDict: Map<number, number[]> = new Map([[0, uniqueCoordList1], [1, uniqueCoordList2], [2, uniqueCoordList3]]);
-			currentXUniqueList = axisDict.get(currentAxisNumber) as number[]
-			currentYUniqueList = axisDict.get(nextAxisNumberDict.get(currentAxisNumber) as number) as number[]
-			currentZUniqueList = axisDict.get(nextAxisNumberDict.get(nextAxisNumberDict.get(currentAxisNumber) as number) as number) as number[]
-			currentXCoord = currentXUniqueList[xLoopCounter]
-			currentYCoord = currentYUniqueList[yLoopCounter]
-			currentZCoord = currentZUniqueList[zLoopCounter]
-
-			const targetPoemDict: Map<number, number[]> = new Map([[0, [currentXCoord, currentYCoord, currentZCoord]], [1, [currentZCoord, currentXCoord, currentYCoord]], [2, [currentYCoord, currentZCoord, currentXCoord]]]);
-			currentTargetPoem = targetPoemDict.get(currentAxisNumber) as number[]
-			
-			if (!this.checkArrIn2dMatrix(outputList, currentTargetPoem) && this.checkArrIn2dMatrix(poemList, currentTargetPoem)) {
-				[uniqueCoordList1, uniqueCoordList2, uniqueCoordList3]= this.updateUniqueLists(poemList, currentTargetPoem);
-				
-				currentAxisNumber = nextAxisNumberDict.get(currentAxisNumber) as number
-				outputList.push(currentTargetPoem.slice(0));
-				poemList = this.removeItem(poemList, currentTargetPoem);
-
-				[xLoopCounter, yLoopCounter, zLoopCounter] = [0, 0, 0];
-				successCounter++
-				 
-			} else if (currentXCoord == currentXUniqueList[currentXUniqueList.length - 1]) {
-				if (currentYCoord == currentYUniqueList[currentYUniqueList.length - 1]) {
-					if (currentZCoord == currentZUniqueList[currentZUniqueList.length - 1]) {
-						zLoopCounter  = 0;
-						yLoopCounter = 0; 
-						xLoopCounter = 0;
-					}
-					zLoopCounter += 1;
-					yLoopCounter = 0;
-					xLoopCounter = 0;
-				}
-				yLoopCounter += 1;
-				xLoopCounter = 0;
-			} else {
-				xLoopCounter += 1;
-			}
-		}
-		if (poemOrder == "end") {
-			outputList = outputList.reverse();
-		}
-		const finalOutputList = outputList.map(value => value.join("-").toString())		
-		return finalOutputList
-	}
-
-	updateUniqueLists(poemList: number[][], currentPoem: number[]) {
-		const uniqueCoordList1Raw: number[] = this.sortedUniqueList(poemList, 0)
-		const uniqueCoordList2Raw: number[] = this.sortedUniqueList(poemList, 1)
-		const uniqueCoordList3Raw: number[] = this.sortedUniqueList(poemList, 2)
-
-		const xIndex = uniqueCoordList1Raw.indexOf(currentPoem[0])
-		const yIndex = uniqueCoordList2Raw.indexOf(currentPoem[1]) 
-		const zIndex = uniqueCoordList3Raw.indexOf(currentPoem[2])
-
-		const uniqueCoordList1: number[] = uniqueCoordList1Raw.slice(xIndex).concat(uniqueCoordList1Raw.slice(0, xIndex));
-		const uniqueCoordList2: number[] = uniqueCoordList2Raw.slice(yIndex).concat(uniqueCoordList2Raw.slice(0, yIndex));
-		const uniqueCoordList3: number[] = uniqueCoordList3Raw.slice(zIndex).concat(uniqueCoordList3Raw.slice(0, zIndex));
-		return [uniqueCoordList1, uniqueCoordList2, uniqueCoordList3]
-	}
-
-	checkArrIn2dMatrix(matrix: number[][], testArr: number[]) {
-		const matrixStr = matrix.map((poemCode) => String(poemCode));	
-		return matrixStr.includes(testArr.toString())
-	}
-
-	removeItem(arr: number[][], item: number[]){
-		return arr.filter(f => f.toString() !== item.toString())
-	}
-
-	async createTemplateList(poemList: string[]) {		
-		const outputList: IPoem[] = [];
-		for (let index = 0; index < poemList.length; index++) {	
-			
-			let hasTextVar = true;
-			const currentPoemName = poemList[index] 
-
-			const currentXml = await this.appService.getPoemXml(currentPoemName).toPromise();
-			const currentGlyph = await this.appService.getPoemGlyph(currentPoemName).toPromise();
-
-			const encodedData = 'data:image/jpeg;base64,' + Buffer.from(currentGlyph).toString('base64')
-			const parser = new DOMParser();
-			const poemXml = parser.parseFromString(currentXml, "text/xml");
-
-			const poemText = poemXml.getElementsByTagName("text")[0].textContent
-			console.log(poemText);
-			
-			let poemTitle = poemXml.getElementsByTagName("title")[0].textContent
-			if (poemTitle == "") {
-				poemTitle = currentPoemName
-			}
-
-			if (poemText === null) {
-					hasTextVar = false;
-			}
-			outputList.push({
-				poemCode: currentPoemName,
-				poemTitle: poemTitle as string,
-				poemText: poemText as string,
-				poemGlyph: encodedData,
-				hasTextVar: hasTextVar,
-			})
-		}				
-		return outputList
-	}
-
-	writeDocument(outputList: IPoem[]) {
-	}
-
-	sortByMultipleValues(inputList: number[][]): number[][] {
-		const outputListRaw: number[][] = [];
-		const tempList = [];
-		const uniqueCoordList1: number[] = this.sortedUniqueList(inputList, 0)
-		const uniqueCoordList2: number[] = this.sortedUniqueList(inputList, 1)
-		const uniqueCoordList3: number[] = this.sortedUniqueList(inputList, 2)
-
-		for (let xj=0; xj < uniqueCoordList1.length; xj++) {
-				for (let yj=0; yj < uniqueCoordList2.length; yj++) {
-						for (let zj=0; zj < uniqueCoordList3.length; zj++) {
-								for (let i=0; i < inputList.length; i++) {
-										if (inputList[i][0] == uniqueCoordList1[xj] && inputList[i][1] == uniqueCoordList2[yj] && inputList[i][2] == uniqueCoordList3[zj]) {
-												tempList.push(inputList[i]);
-										}
-								}
-						}
-				}
-		}
-		const outputList = outputListRaw.concat(tempList);
-	return outputList
-	}
-
-	sortedUniqueList(inputList: number[][], coordIndex: number) {
-		const currentCoordSet = new Set(inputList.map(poemCode => poemCode[coordIndex]));  
-		const currentCoordUnique = (Array.from(currentCoordSet));		
-		const finalUniqueList = currentCoordUnique.sort(function(a, b){return a - b});
-		return finalUniqueList
 	}
 
 	sleep(ms: number) { 
